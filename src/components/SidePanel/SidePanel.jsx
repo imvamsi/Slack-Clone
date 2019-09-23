6fb5aa2607dc8e1dbf9e57900faf68cb07697fcd
imvamsi <krrishvamsi7@gmail.com>
2019-09-23 15:18:21 -0400

    added user profile dropdown box :fire:

--- src/components/SidePanel/SidePanel.jsx
+++ src/components/SidePanel/SidePanel.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { Menu } from "semantic-ui-react";
+import UserPanel from "./UserPanel";
 const SidePanel = () => {
-  return <div>Sidepanel</div>;
+  return (
+    <Menu
+      size="large"
+      inverted
+      fixed="left"
+      vertical
+      style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
+    >
+      <UserPanel />
+    </Menu>
+  );
 };
 
 export default SidePanel;
---
+++ src/components/SidePanel/UserPanel.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Grid, Header, Icon, Dropdown } from "semantic-ui-react";
+const UserPanel = () => {
+  const dropDownOptions = () => [
+    {
+      id: "user",
+      text: (
+        <span>
+          Logged in as <strong>user</strong>
+        </span>
+      ),
+      disabled: true
+    },
+    {
+      key: "avatar",
+      text: <span>Change Avatar</span>
+    },
+    {
+      key: "sign out",
+      text: <span>Sign Out</span>
+    }
+  ];
+  return (
+    <Grid style={{ background: "#4c3c4c" }}>
+      <Grid.Column>
+        <Grid.Row style={{ padding: "1.2em", margin: "0" }}>
+          <Header inverted as="h2" floated="left">
+            <Icon name="code" />
+            {/* app header */}
+            <Header.Content>Chatbox</Header.Content>
+          </Header>
+        </Grid.Row>
+
+        <Header style={{ padding: "0.25em" }} as="h4" inverted>
+          <Dropdown trigger={<span>user</span>} options={dropDownOptions()} />
+        </Header>
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default UserPanel;

f80c18fec081c2b302cc8aea95bda817a24304cb
imvamsi <krrishvamsi7@gmail.com>
2019-09-24 18:41:03 -0400

    added channel reducer :art:

--- src/actions/Actions.js
+++ src/actions/Actions.js
@@ -1,4 +1,4 @@
-import { SET_USER, CLEAR_USER } from "./types";
+import { SET_USER, CLEAR_USER, SET_CURRENT_CHANNEL } from "./types";
 
 export const setUser = user => async dispatch => {
   dispatch({
@@ -14,3 +14,43 @@ export const clearUser = () => async dispatch => {
     type: CLEAR_USER
   });
 };
+
+// export const setCurrentChannel = channel =>  {
+//   return {
+//     type: SET_CURRENT_CHANNEL,
+//     payload: {
+//       currentChannel: channel
+//     }
+//   };
+// };
+
+export const setCurrentChannel = channel => async dispatch => {
+  dispatch({
+    type: SET_CURRENT_CHANNEL,
+    payload: {
+      currentChannel: channel
+    }
+  });
+};
+
+// export function setCurrency() {
+//   return function(dispatch) {
+//     return axios
+//       .get("some-url")
+//       .then(responseData => {
+//         dispatch(setCurrencysuccess(responseData));
+//       })
+//       .catch((error) => {
+//         dispatch(setCurrencyErro(responseData));
+//       });
+//   };
+// }
+
+// export function setCurrencysuccess(){
+//   return {
+//     type: SET_CURRENT_CHANNEL,
+//     payload: {
+//       currentChannel: channel
+//     }
+//   };
+// }
